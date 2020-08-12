5170003f908e681986d0a9d4ac42b77b953ec57c
bernie <mail@javaner.de>
2020-08-12 16:02:18 +0200

    privat functions

--- src/DragControl.js
+++ src/DragControl.js
@@ -1,82 +1,95 @@
 import {Vector3} from "../node_modules/three/build/three.module.js";
 import Intersectionplane from "./Intersectionplane.js";
 
+const onDragstart = function( ev ){
+    
+    let object3d = ev.target;
+
+    ev.origDomEvent.preventDefault();
+    ev.origDomEvent.stopPropagation();
+
+    this._domElement.style.cursor = 'move';
+    this._draggingObjects[object3d.id] = object3d;
+
+    this.iplane.setPlaneDirectionAndPositon( object3d, ev.intersect, this._dirs[object3d.id] );
+    this.iplane.setOffset( object3d );
+};
+
+const onDrag = function( ev ) {
+    ev.origDomEvent.preventDefault();
+    ev.origDomEvent.stopPropagation();
+    
+    this.iplane.setPosition( ev.target );
+};
+
+const onDragend = function( ev ){
+    this._domElement.style.cursor = 'auto';
+    delete this._draggingObjects[ev.target.id];
+};
+
 
 const DragControl = function( domevents ) {
+
+    let scope = this;
     this.iplane = new Intersectionplane( domevents._camera, domevents._ray );
     this._domElement = domevents._domElement;
     this._dirs = {};
-    this._draggingObject = {};
+    this._draggingObjects = {};
 
-    this._onMouseCancle = this.onMouseCancle.bind(this);
-    this._mousemove = this.mousemove.bind(this);
-    this._mousedown = this.mousedown.bind(this);
+    this._domElement.addEventListener("mousemove", function( ev ){
+		if ( Object.keys( this._draggingObjects ).length > 0 ){
+			ev.preventDefault();
+			ev.stopPropagation();
+		}
+	}.bind( this ) );
+
+    this._dragStart = function( ev ){
+        onDragstart.call( scope, ev );
+    };
+
+    this._onDrag = function( ev ){
+        onDrag.call( scope, ev );
+    };
+
+    this._onDragend = function( ev ){
+        onDragend.call( scope, ev );
+    };
 };
 
 Object.assign( DragControl.prototype, {
 
-    make : function( object3d, dir ){ 
+    enableDraggable : function( object3d, dir ){ 
 
         let scope = this;
 
-        this._dirs[object3d.id] = dir || "cam";
-
-        object3d.addEventListener("click", function( ev ){
-            console.log( ev );
-        });
-
-        object3d.addEventListener("mousedown", function( ev ){ 
-            scope._mousedown( ev, object3d );
-        });
-
-        object3d.addEventListener("mousemove", function( ev ){           
-            scope._mousemove( ev, object3d );
-        });
-
-        object3d.addEventListener("mouseup", function( ev ){ 
-            scope._onMouseCancle( ev, object3d ); 
-        });
-        object3d.addEventListener("mouseout", function( ev ){ 
-            scope._onMouseCancle( ev, object3d ); 
-        });
-    },
-
-    mousedown : function( ev, object3d ){ 
-        ev.origDomEvent.preventDefault();
-        ev.origDomEvent.stopPropagation();
-
-        this.iplane.setOffset( object3d );
-
-        this._draggingObject[ object3d.id ] = object3d;
-        
-        this._domElement.style.cursor = 'move';
-
-        object3d.dispatchEvent( { type: 'dragstart', object: ev } );
-        console.log("dragstart", object3d);
-    },
-
-    mousemove : function( ev, object3d ){
-            
-        ev.origDomEvent.preventDefault();
-        ev.origDomEvent.stopPropagation();
-
-        if ( this._draggingObject[ object3d.id ] ) {
-            
-            this.iplane.setPosition( object3d );
-
-            object3d.dispatchEvent( { type: 'drag', object: ev } );
-        
-            return;
+        if( this.isDraggable( object3d ) ){
+            this.disableDraggable( object3d );
         }
 
-        this.iplane.setPlaneDirectionAndPositon( object3d, ev.intersect, "xz" );
+        this._dirs[object3d.id] = dir || "cam";
+
+        object3d.addEventListener("dragstart", this._dragStart);
+        object3d.addEventListener("drag", this._onDrag );
+        object3d.addEventListener("dragend", this._onDragend );
+
     },
 
-    onMouseCancle : function( ev, object3d ){
-        this._draggingObject[ object3d.id ] = null;
-        this._domElement.style.cursor = 'auto';
-        object3d.dispatchEvent( { type: 'dragstop', object: ev } );
-        console.log("dragend");
+    disableDraggable : function( object3d ){
+
+        object3d.removeEventListener("dragstart", this._dragStart);
+        object3d.removeEventListener("drag", this._onDrag );
+        object3d.removeEventListener("dragend", this._onDragend );
+
+    },
+
+    isDraggable : function( object3d ){
+
+        return object3d.hasEventListener("drag", this._onDrag );
+
+    },
+
+    pauseDragging : function(){
+        //ToDo
     }
 });
 
